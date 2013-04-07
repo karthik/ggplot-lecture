588f24b23c319fe38ead921a385877f80e6201a4
Karthik Ram <karthik.ram@gmail.com>
2013-04-07 12:04:27 -0700

    Added new beamer theme and some images

--- make.sh
+++ make.sh
@@ -1,3 +1,9 @@
-Rscript -e "library(knitr); knit('ggplot.Rnw')"
+Rscript -e "library(knitr); knit('ggplot.Rnw', quiet = TRUE)"
 pdflatex ggplot.tex
 open ggplot.pdf
+rm *.log
+rm *.nav
+rm *.out
+rm *.snm
+rm *.toc
+rm *.vrb
